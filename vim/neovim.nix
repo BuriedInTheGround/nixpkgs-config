167b35b2a175be3741c9ab7bf8ae649bea2e48ac
BuriedInTheGround <simone99.as@gmail.com>
2020-11-22 22:36:22 +0100

    :lipstick: Add vim theming

--- vim/neovim.nix
+++ vim/neovim.nix
@@ -4,7 +4,32 @@
   programs.neovim = {
     enable = true;
     vimAlias = true;
+    extraConfig = ''
+      set autowrite
+      set termguicolors
+
+      " Colorscheme configuration
+      let g:nord_cursor_line_number_background = 1
+      let g:nord_italic = 1
+      let g:nord_italic_comments = 1
+      colorscheme nord
+
+      " Turn hybrid line numbers on
+      :set number relativenumber
+      :augroup numbertoggle
+      :  autocmd!
+      :  autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
+      :  autocmd BufLeave,FocusLost,InsertEnter   * set norelativenumber
+      :augroup END
+    '';
     plugins = with pkgs.vimPlugins; [
+      # Theming
+      nord-vim
+      vim-airline
+      vim-airline-themes
+
+      # Languages
+      vim-nix
       {
         plugin = vim-go;
 	config = ''
