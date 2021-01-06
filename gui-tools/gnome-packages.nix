33710b0e189667c242a061db857d436fa0a38706
Simone Ragusa <simone99.as@gmail.com>
2021-01-06 22:43:55 +0100

    gui-tools: add gnome-system-monitor and baobab

--- gui-tools/gnome-packages.nix
+++ gui-tools/gnome-packages.nix
@@ -2,8 +2,10 @@
 
 {
   home.packages = [
+    pkgs.gnome3.baobab
     pkgs.gnome3.gnome-calculator
     pkgs.gnome3.gnome-screenshot
+    pkgs.gnome3.gnome-system-monitor
     pkgs.gnome3.nautilus
   ];
 }
