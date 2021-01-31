2d715c0c1004821698bf6ad178572fae589f2fbb
Simone Ragusa <simone99.as@gmail.com>
2021-01-31 23:19:01 +0100

    wm/polybar: add cputemp module + fix monitor issue
    Add cpu temperature polybar module.
    Fix single monitor naming issue by generalizing it.
    Fix broken brightness hotkey (there was an extra space).

--- wm/polybar/launch.sh
+++ wm/polybar/launch.sh
@@ -8,13 +8,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Compose hwmon path
+hwmon_path="/sys/devices/platform/coretemp.0/hwmon/$(ls /sys/devices/platform/coretemp.0/hwmon/)/temp1_input"
+
 # Launch main and secondary bar...
 if [ "$(xrandr --listmonitors | grep 'Monitors: ' | cut -d ' ' -f 2)" -gt 1 ]; then
-    polybar -c ~/.config/polybar/config.ini main & disown
-    polybar -c ~/.config/polybar/config.ini secondary & disown
+    HWMON=$hwmon_path polybar -c ~/.config/polybar/config.ini main & disown
+    HWMON=$hwmon_path polybar -c ~/.config/polybar/config.ini secondary & disown
 else
     # ...or laptop bar if there is only one monitor
-    polybar -c ~/.config/polybar/config.ini laptop & disown
+    MONITOR=$(polybar --list-monitors | cut -d ':' -f 1) HWMON=$hwmon_path polybar -c ~/.config/polybar/config.ini laptop & disown
 fi
 
 # Update lockscreen wallpaper cache
