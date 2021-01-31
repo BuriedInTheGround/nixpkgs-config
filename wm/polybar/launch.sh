0d2a770df32659b1ba0b9892653a3c18ff10161d
Simone Ragusa <simone99.as@gmail.com>
2021-01-31 12:03:47 +0100

    wm/bspwm,wm/polybar: better laptop support
    Distinct between `one` and `more than one` monitor setup.

--- wm/polybar/launch.sh
+++ wm/polybar/launch.sh
@@ -8,9 +8,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch main and secondary bar
-polybar -c ~/.config/polybar/config.ini main & disown
-polybar -c ~/.config/polybar/config.ini secondary & disown
+# Launch main and secondary bar...
+if [ "$(xrandr --listmonitors | grep 'Monitors: ' | cut -d ' ' -f 2)" -gt 1 ]; then
+    polybar -c ~/.config/polybar/config.ini main & disown
+    polybar -c ~/.config/polybar/config.ini secondary & disown
+else
+    # ...or laptop bar if there is only one monitor
+    polybar -c ~/.config/polybar/config.ini laptop & disown
+fi
 
 # Update lockscreen wallpaper cache
 betterlockscreen -u ~/.config/polybar/scripts/lockwallpaper.png &
