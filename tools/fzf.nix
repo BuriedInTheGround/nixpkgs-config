85eed4257b5e14b60d7f81b94f9f4f960c40806a
Simone Ragusa <simone99.as@gmail.com>
2021-03-28 12:41:58 +0200

    tools: set FZF_CTRL_T_OPTS and FZF_ALT_C_OPTS variables
    These two session variables tells fzf how to style the view of `CTRL+T`
    and `ALT+C` terminal commands.

--- tools/fzf.nix
+++ tools/fzf.nix
@@ -4,4 +4,9 @@
   programs.fzf = {
     enable = true;
   };
+
+  home.sessionVariables = {
+    FZF_CTRL_T_OPTS = "--no-height --preview 'bat --color=always --style=numbers --line-range=:500 {}'";
+    FZF_ALT_C_OPTS = "--preview 'tree -c {} | head -200'";
+  };
 }
