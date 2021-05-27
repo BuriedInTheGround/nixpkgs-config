4f51324ff67b4885fdaec45387c8f4c812b8cb04
Simone Ragusa <simone99.as@gmail.com>
2021-05-27 21:04:44 +0200

    wm/polybar: remove wallpaper cache update on polybar launch

--- wm/polybar/launch.sh
+++ wm/polybar/launch.sh
@@ -8,19 +8,20 @@ hwmon_path="/sys/devices/platform/coretemp.0/hwmon/$(ls /sys/devices/platform/co
 
 # Launch main and secondary bar...
 if [ "$(xrandr --listmonitors | rg 'Monitors: ' | cut -d ' ' -f 2)" -gt 1 ]; then
-    HWMON=$hwmon_path polybar -c ~/.config/polybar/config.ini main & disown
-    HWMON=$hwmon_path polybar -c ~/.config/polybar/config.ini secondary & disown
+    HWMON=$hwmon_path polybar --reload secondary -c ~/.config/polybar/config.ini & disown
+    sleep 0.2
+    HWMON=$hwmon_path polybar --reload main -c ~/.config/polybar/config.ini & disown
 else
     # ...or laptop bar if there is only one monitor
     MONITOR=$(polybar --list-monitors | cut -d ':' -f 1) HWMON=$hwmon_path polybar -c ~/.config/polybar/config.ini laptop & disown
 fi
 
 # Update lockscreen wallpaper cache
-multilockscreen -u ~/.config/polybar/scripts/lockwallpaper.png --fx dim,color --display 1 --span &
+# multilockscreen -u ~/.config/polybar/scripts/lockwallpaper.png --fx dim,color --display 1 --span &
 
 # Set the locker
-xss-lock -l -- multilockscreen --lock dim --display 1 --span &
+xss-lock -l -- multilockscreen --lock dim --display 1 --span & disown
 
 # Run auto-toggle-polybar script for hiding the bar when in fullscreen node
 kill $(pgrep -u $UID -f 'auto-toggle-polybar.sh')
-~/.config/polybar/scripts/auto-toggle-polybar.sh &
+~/.config/polybar/scripts/auto-toggle-polybar.sh & disown
