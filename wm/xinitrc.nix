56d50e9daef9ee6ee5f67c8acbd6b08afe4b13d3
Simone Ragusa <simone99.as@gmail.com>
2021-07-27 18:42:19 +0200

    wm: fix typo

--- wm/xinitrc.nix
+++ wm/xinitrc.nix
@@ -2,7 +2,7 @@
 
 {
   home.file.".xinitrc".text = ''
-    eval $(/run/wrappers/bin/gnome-keyring-daemon --start --deamonize --components=pkcs11,secrets,ssh)
+    eval $(/run/wrappers/bin/gnome-keyring-daemon --start --daemonize --components=pkcs11,secrets,ssh)
     export SSH_AUTH_SOCK
   '';
 }
